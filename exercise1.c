a829ec9f18ffae3784f485b2e746d424d7e40417
feynman <feynman@ri-l16778-1.win.dtu.dk>
2016-09-13 11:24:57 +0200

    rename

---
+++ exercise1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	static const char filename[] = "ECG.txt";
+	FILE *file = fopen(filename, "r");
+	int value = 1, max_value;
+	fscanf(file, "%d", &max_value);
+
+	while(!feof(file)){
+		fscanf(file, "%d", &value);
+		if(value > max_value){
+			max_value = value;
+
+		}
+	}
+	printf("%d", max_value);
+
+
+	return 0;
+}
