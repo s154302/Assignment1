1742de7a93e1371220936ce7b54446b9e2948ad8
feynman <feynman@food-w1-satue-01.win.dtu.dk>
2016-09-13 13:56:38 +0200

    data_array

--- filters.c
+++ filters.c
@@ -5,8 +5,16 @@
 // Feel free to change return statement and arguments
 int lowPassFilter(int data) {
     static int count, previous_1, previous_2, data_6, data_12, current;
+    int data_array[12];
     
-    current = 2 * previous_1 - previous_2 + (data-2*data_6+data_12) / 32;
+    current = 2 * previous_1 - previous_2 + (data-2*data_array[5]+data_array[11]) / 32;
+    previous_2 = previous_1;
+    previous_1 = current;
+
+    for(int i = 11; i>=0;i--){
+    	data_array[i] = data_array[i-1];
+    }
+	data_array[0] = data;
 
     return current;
 }
