d06e28eb72c2d3bd755d6a0620ac4257b6306e37
Computer <Computer@DESKTOP-OT5H4O0>
2016-09-20 11:10:09 +0200

    Peak Detection
    Attempted implementation of peak detection. Couldn't figure out a good
    way to store the peaks. At the moment they are stored in an array of
    size 100. Figured we wouldn't be searching further back than that in the
    searchback.

--- main.c
+++ main.c
@@ -12,9 +12,9 @@ int main() {
 	QRS_params qsr_params; // Instance of the made avaiable through: #include "qsr.h"
 	FILE *file;                  // Pointer to a file object
 	file = openfile("ECG.txt");
-	int data, peak_determination[3];
+	int data, peak_determination[3], peaks[100];
 
-	//    while(!feof(file)) {
+	while(!feof(file)) {
 	for (int i = 0; i < 200; i++) {
 		data = filterData(data);
 
@@ -22,6 +22,20 @@ int main() {
 			peak_determination[i] = peak_determination[i - 1];
 		}
 		peak_determination[0] = data;
+
+		// Below needs revision.
+
+		if(peak_determination[1] > peak_determination[0] && peak_determination[1] > peak_determination[2]) {
+			for(int i = 99; i > 1; i--) {
+				peaks[i] = peaks[i - 1];
+			}
+			peaks[0] = peak_determination[1];
+
+			struct QRS_params peak;
+
+
+		}
+
 
 	}
 
