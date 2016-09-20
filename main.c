5657ada3f7b85cc3923632bfaff61a31abf2a8fc
feynman <feynman@dtu-hljvvz1v.win.dtu.dk>
2016-09-20 13:00:37 +0200

    refactoring and minor changes

--- main.c
+++ main.c
@@ -12,34 +12,21 @@ int main() {
 	QRS_params qsr_params; // Instance of the made avaiable through: #include "qsr.h"
 	FILE *file;                  // Pointer to a file object
 	file = openfile("ECG.txt");
-	int data, peak_determination[3], peaks[100];
+	int data;
 
-	while(!feof(file)) {
-	for (int i = 0; i < 200; i++) {
-		data = filterData(data);
+	while (!feof(file)) {
+		//filtering and finding data
+		data = filterData(getNextData(file));
 
-		for (int i = 2; i > 0; i--) {
-			peak_determination[i] = peak_determination[i - 1];
-		}
-		peak_determination[0] = data;
+		//finding peak
+		findingPeaks(data);
 
-		// Below needs revision.
-
-		if(peak_determination[1] > peak_determination[0] && peak_determination[1] > peak_determination[2]) {
-			for(int i = 99; i > 1; i--) {
-				peaks[i] = peaks[i - 1];
-			}
-			peaks[0] = peak_determination[1];
-
-			struct QRS_params peak;
-
-
-		}
-
+		struct QRS_params peak;
 
 	}
 
 //	peakDetection(&qsr_params); // Perform Peak Detection
 
 	return 0;
+
 }
